ca083bd40060ba4d5db6386b8f7ba6b28f7c256f
salatield206 <salatield206@gmail.com>
2026-09-15 23:30:03 -0300

    Corrigindo zoom dos inputs no mobile

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,12 @@ export const metadata = {
   description: 'Repositório universal de estudos'
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1
+};
+
 export default function RootLayout({ children }) {
   return <html lang="pt-BR"><body><Providers>{children}</Providers></body></html>;
 }
